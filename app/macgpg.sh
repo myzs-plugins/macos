779e2f8199fb6aee5122ddb533e9f8f209c16b40
Kamontat Chantrachirathumrong <kamontat.chantrachirathumrong@agoda.com>
2021-06-09 15:27:57 +0700

    chore(release): automatic commit process

--- app/macgpg.sh
+++ app/macgpg.sh
@@ -1,6 +1,6 @@
 # shellcheck disable=SC1090,SC2148
 
-_myzs:internal:module:initial "$0"
+myzs:module:new "$0"
 
 export MACGPG_ROOT="/usr/local/MacGPG2"
 if _myzs:internal:checker:folder-exist "$MACGPG_ROOT"; then
