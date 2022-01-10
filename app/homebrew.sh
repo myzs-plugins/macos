057e6c5cdc5002e3f664165563d977415bb4bcdc
Kamontat Chantrachirathumrong <developer@kamontat.net>
2022-01-10 13:49:28 +0700

    fix(util): disable analysis of homebrew usage

--- app/homebrew.sh
+++ app/homebrew.sh
@@ -4,6 +4,6 @@ myzs:module:new "$0"
 
 _homebrew="/opt/homebrew/bin/brew"
 if _myzs:internal:checker:file-exist "$_homebrew"; then
-  export HOMEBREW_NO_ANALYTICS=1
   eval "$(/opt/homebrew/bin/brew shellenv)"
+  "$_homebrew" analytics off
 fi
