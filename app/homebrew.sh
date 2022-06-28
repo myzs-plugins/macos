d378f24e17d29beff202e945992f13a9eb045d2c
Kamontat Chantrachirathumrong <developer@kamontat.net>
2022-06-28 15:06:32 +0700

    chore(release): automatic commit process

--- app/homebrew.sh
+++ app/homebrew.sh
@@ -4,6 +4,10 @@ myzs:module:new "$0"
 
 _homebrew="/opt/homebrew/bin/brew"
 if _myzs:internal:checker:file-exist "$_homebrew"; then
-  eval "$(/opt/homebrew/bin/brew shellenv)"
-  "$_homebrew" analytics off
+  eval "$("$_homebrew" shellenv)"
+
+  _myzs:internal:fpath-push "/opt/homebrew/share/zsh/site-functions"
+
+  export HOMEBREW_NO_ANALYTICS=1
+  # "$_homebrew" analytics off
 fi
